56424f32ce7b607a30feb399e328955d6d783ea9
Joanna Ruhl <ms.jo.ruhl@gmail.com>
2024-10-03 14:14:55 -0400

    Add files via upload

---
+++ FilterCharacters.py
@@ -0,0 +1,10 @@
+string="fiifnyd.tchoemh/itdrdaecnka/n6i0mHa2lc:3h1tqtQpMsD:F/5/towpyeTnb.Zs3pxoat"
+newstring=""
+
+for i in range(73):
+  if i%2 ==0:
+    newstring+= string[i]
+for j in range(73):
+  if j%2!=0:
+    newstring+= string[j]
+print(newstring)
